f9bedce17e2411e988a7a5d608e4ae96c4aeaba0
CISAC <carolisacosta99@gmail.com>
2022-11-04 13:47:16 +0100

    Fixed refuel bug

--- Assets/_Personals/Carolina/Scripts/RefuelStationBehaviour.cs
+++ Assets/_Personals/Carolina/Scripts/RefuelStationBehaviour.cs
@@ -1,8 +1,12 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class RefuelStationBehaviour : MonoBehaviour
 {
-
+    private void OnDisable()
+    {
+        GameManager.Instance.GroundGenerator.activeFuelStations.Remove(this.gameObject);
+    }
 }
